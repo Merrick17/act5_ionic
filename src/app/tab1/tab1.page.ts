1724f80e7853326549796ed43fce8f67930218b4
safwen <safwen.benfredj@gmail.com>
2020-04-28 15:22:31 +0100

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import * as data from "../../assets/users.json";
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"],
 })
-export class Tab1Page {
-
+export class Tab1Page implements OnInit {
+  users = [];
   constructor() {}
-
+  ngOnInit(): void {
+    console.log(data.users);
+    this.users = data.users;
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UsersService } from "../users.service";
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: "app-tab2",
+  templateUrl: "tab2.page.html",
+  styleUrls: ["tab2.page.scss"],
 })
-export class Tab2Page {
-
-  constructor() {}
-
+export class Tab2Page implements OnInit {
+  constructor(private userService: UsersService) {}
+  users = [];
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((data) => {
+      let result: any = data;
+      this.users = result.users;
+      console.log(result);
+    });
+  }
 }
